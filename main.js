6b20e5a56f797ddffc97fe4c2ebd82ab10edb8d0
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-18 12:00:48 -0400

    second commit 100%

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-// 1. First, find our UL Container
+const characters = document.querySelector('ul')
+const char= document.createElement('li')
 
-// 2. Create our Ajax Request
+	const grego = fetch('http://swapi.co/api/people/').then(response => response.json()).then(grego =>{
+  for (var i=0; i <grego.results.length; i++){
+
+  let char= document.createElement('li')
+  char.innerHTML += grego.results[i].name
+  characters.appendChild(char)
+  }})
